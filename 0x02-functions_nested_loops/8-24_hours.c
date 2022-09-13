9307ab304ff4a50670369de855c0dadbd141dbcb
Marshal Nzenza <mnzenza@zimswitch.co.zw>
2022-09-14 01:36:18 +0200

    task 0 to 11 finished

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,22 @@
-#include <stdio.h>
-#include "main.h"
+#include"main.h"
+
 /**
- * jack_bauer -  function that prints every minute of
- * the day of Jack Bauer, starting from 00:00 to 23:59.
- * Return: 0
- */
+ * jack_bauer- prints every minute of the day
+*/
+
 void jack_bauer(void)
 {
-	int i, j;
+	int min, hr;
 
-	for (i = 0; i < 60; i++)
+	for (hr = 0; hr <= 23; ++hr)
 	{
-		for (j = 0; j < 24; j++)
+		for (min = 0; min <= 59; ++min)
 		{
-			_putchar(i / 10 + 48);
-			_putchar(i % 10 + 48);
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
 			_putchar(':');
-			_putchar(j / 10 + 48);
-			_putchar(j % 10 + 48);
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 			_putchar('\n');
 		}
 	}
